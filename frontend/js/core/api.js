dee11114aadb249fe3e301b4997bc3ad3c0b073f
Sweranzo <jeffcarandang5@gmail.com>
2026-07-03 18:54:29 +0800

    modify: API URL to render link

--- frontend/js/core/api.js
+++ frontend/js/core/api.js
@@ -1,5 +1,4 @@
-const API_BASE_URL = "http://localhost:5000/api";
-
+const API_BASE_URL ="https://disciplinary-system.onrender.com";
 const AppProgress = (() => {
   let activeRequests = 0;
   let progressValue = 0;
