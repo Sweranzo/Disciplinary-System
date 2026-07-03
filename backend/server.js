9397510a7c732970e5be21a2b592aa06be00b4de
Sweranzo <jeffcarandang5@gmail.com>
2026-07-03 18:35:19 +0800

    Update: app.listen log more detail

--- backend/server.js
+++ backend/server.js
@@ -55,5 +55,5 @@ app.use((req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
